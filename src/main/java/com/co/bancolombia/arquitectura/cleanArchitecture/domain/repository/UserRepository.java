b1f01d550c57c3d5cd3200e415e09aeee308dd70
LinaDev14 <perfilgitlinamaria@gmail.com>
2024-04-07 21:34:57 -0500

    readme.md

--- src/main/java/com/co/bancolombia/arquitectura/cleanArchitecture/domain/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.co.bancolombia.arquitectura.cleanArchitecture.domain.repository;
-
-import com.co.bancolombia.arquitectura.cleanArchitecture.domain.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
